bb68a2818905325b81566198073a5db911a693f0
MatousMarik <matymar@email.cz>
2022-05-29 17:56:12 +0200

    abstract agent

--- dino/game/agent.py
+++ dino/game/agent.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 from game.dino import Game, DinoMove
+from abc import ABC, abstractmethod
 
 
-class Agent:
+class Agent(ABC):
     """
     Abstract class for dino reflex agent implementation.
 
     Note that you should not use any initialization since this is reflex agent.
     """
 
+    @abstractmethod
     def get_move(self, game: Game) -> DinoMove:
-        raise RuntimeError("Calling abstract class method.")
+        pass
